da83374ab736df138fdde8a561c454fcccca823b
Pius Ezekiel <pius@Devil-PC>
2023-09-16 14:09:45 +0200

    0-minoperations.py

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -1,20 +1,23 @@
 #!/usr/bin/python3
 """
-a method called minOperations(n) that calculates the fewest number of operations needed to 
+a method called minOperations(n) that calculates
+the fewest number of operations needed to
 result in exactly n H characters in a text file.
 """
+
+
 def minOperations(n):
     if n <= 1:
-        return 0  # If n is less than or equal to 1, no operations are needed, so return 0
+        return 0
 
-    operations = 0  # Initialize the number of operations to 0
-    factor = 2  # Start with the smallest prime factor
+    operations = 0
+    factor = 2
 
     while factor <= n:
-        if n % factor == 0:  # Check if the current factor divides n evenly
-            operations += factor  # Add the factor to the number of operations
-            n //= factor  # Divide n by the factor to reduce it
+        if n % factor == 0:
+            operations += factor
+            n //= factor
         else:
-            factor += 1  # If the current factor does not divide n, move to the next factor
+            factor += 1
 
-    return operations  # Return the total number of operations needed
+    return operations
